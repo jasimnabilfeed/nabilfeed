3b54fd72aefb2b8f4731f583f8bfe4b89c850ec9
Jasim Uddin <jasimnabilfeed@gmail.com>
2025-11-05 23:59:38 +0600

    .

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <h3>Welcome To Nabil Feed Mills Limited.</h3>
       <main className={styles.main}>
         <Image
           className={styles.logo}

f0b102d53c9ce421dc22d1e44fd2ede591a7f5e5
Jasim Uddin <jasimnabilfeed@gmail.com>
2025-11-17 00:25:02 +0600

    .

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function Products() {
     return (
         <div>
             <h2 className="text-3xl font-bold mb-4">Our Products</h2>
+            <ul className="hidden md:flex gap-6 font-medium">
+                <li><Link href="/teams/head-office">Head Office Team</Link></li>
+                <li><Link href="/teams/north-zone">Rajshahi-Rangpur Team</Link></li>
+                <li><Link href="/teams/south-zone">South Bengal Team</Link></li>
+                <li><Link href="/teams/central-zone">Central Zone Team</Link></li>
+                <li><Link href="/teams/east-zone">East Zone Team</Link></li>
+            </ul>
             <p className="text-gray-700">List your products here.</p>
         </div>
     );

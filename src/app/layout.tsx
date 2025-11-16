b4972093ac3f2e36ead25aca9d192c27f57dcab9
Jasim Uddin <jasimnabilfeed@gmail.com>
2025-11-16 22:03:11 +0600

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Navel Feed Mills",
@@ -25,10 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className="bg-gray-50">
+        <Navbar />
+        <main className="container mx-auto py-10">{children}</main>
+        <Footer />
       </body>
     </html>
   );

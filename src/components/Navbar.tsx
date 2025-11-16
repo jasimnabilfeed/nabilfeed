fba3986e0c0757610658d5fa4cf9b20510d14597
Jasim Uddin <jasimnabilfeed@gmail.com>
2025-11-16 22:24:26 +0600

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,16 +4,21 @@ export default function Navbar() {
     return (
         <nav className="p-4 shadow bg-white sticky top-0 z-50">
             <div className="container mx-auto flex justify-between items-center">
-                <h1 className="text-xl font-bold">Company</h1>
-                <ul className="flex gap-6 font-medium">
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/products">Products</Link></li>
-                    <li><Link href="/teams">Our Team</Link></li>
-                    <li><Link href="/gallery">Gallery</Link></li>
-                    <li><Link href="/mission-vision">Mission & Vision</Link></li>
-                    <li><Link href="/careers">Career</Link></li>
-                    <li><Link href="/contact-us">Contact Us</Link></li>
-                </ul>
+                <div>
+                    <Link href="/">
+                        <h1 className="text-xl font-bold">Company</h1>
+                    </Link>
+
+                </div>
+                <div>
+                    <ul className="flex gap-6 font-medium">
+                        <li><Link href="/products">Products</Link></li>
+                        <li><Link href="/teams">Our Team</Link></li>
+                        <li><Link href="/gallery">Gallery</Link></li>
+                        <li><Link href="/careers">Career</Link></li>
+                        <li><Link href="/contact-us">Contact Us</Link></li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
